2f6f5ad83c6c5a1705c15138757a79847fb2bef5
Paul English <paul@onfrst.com>
2017-07-26 14:21:49 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Coffee Bot
+
+Script that watches an EdiMax energy monitoring plug for current draw, and triggers a slack message when a pot of coffee has finished brewing.
---
+++ main.py
@@ -0,0 +1,36 @@
+import time
+import os
+from datetime import datetime
+
+import redis
+
+from smartplug import SmartPlug
+
+REDIS_URL = os.environ.get('REDIS_URL', 'localhost')
+PLUG_IP = '172.16.100.75'
+PLUG_USER = 'admin'
+PLUG_PW = '1234'
+
+# Regularly log current & power draw so we can estimate
+# a heuristic rule for when the coffee is finished after
+# we've seen it run
+def main():
+    print('Starting Coffee Bot')
+
+    r = redis.StrictRedis(host=REDIS_URL)
+    plug = SmartPlug(PLUG_IP, (PLUG_USER, PLUG_PW))
+
+    while True:
+        now = datetime.now().timestamp()
+        try:
+            power = plug.power
+            current = plug.current
+            print('Plug', now, plug.state, power, current)
+            r.zadd('coffee_bot_stats', now, json.dumps({'power': power, 'current': current}))
+        except Exception as e:
+            print('Error', now, str(e))
+        print('Sleeping')
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
